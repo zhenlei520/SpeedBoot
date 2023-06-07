174f53e88121ffbf859cd41c67e16d645e253738
zhenlei520 <wangzhenlei520@gmail.com>
2023-06-08 02:54:23 +0800

    feat: Add UnitTest

---
+++ test/Speed.System.Tests/SpeedExceptionTest.cs
@@ -0,0 +1,14 @@
+using GeneratorSource;
+
+namespace Speed.System.Tests;
+
+[TestClass]
+public class SpeedExceptionTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var exceptionMessage = SpeedExceptionGenerator.GetExceptionMessage();
+        Assert.AreEqual(10, exceptionMessage.Count);
+    }
+}
