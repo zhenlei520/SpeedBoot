cbc19f962c469a1efeafe3c6e4d924c5f835d27a
zhenlei520 <wangzhenlei520@gmail.com>
2023-11-13 12:10:24 +0800

    feat: support mysql database

---
+++ src/Data/Speed.Boot.Data.EFCore.Pomelo.MySql/Using.cs
@@ -0,0 +1,7 @@
+﻿global using System.Data.Common;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Infrastructure;
+global using Microsoft.Extensions.DependencyInjection;
+global using Pomelo.EntityFrameworkCore.MySql.Storage;
+global using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
+global using Speed.Boot.Data.Abstractions;
