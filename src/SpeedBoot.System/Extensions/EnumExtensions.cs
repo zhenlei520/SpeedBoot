8ae5e3610d3b1c236f4636fb36655c8da34dedae
zhenlei520 <wangzhenlei520@gmail.com>
2024-03-01 01:07:34 +0800

    feat: add Enum Extensions

---
+++ src/SpeedBoot.System/Extensions/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) zhenlei520 All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace SpeedBoot.System;
+
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value, string defaultValue = "")
+    {
+        value.TryGetDescription(out var description, defaultValue);
+        return description;
+    }
+
+    public static bool TryGetDescription(this Enum value, out string description, string defaultValue = "")
+    {
+        var attribute = value.GetCustomAttribute<DescriptionAttribute>();
+        if (attribute == null)
+        {
+            description = defaultValue;
+            return false;
+        }
+        description = attribute.Description;
+        return true;
+    }
+
+    public static TAttribute? GetCustomAttribute<TAttribute>(this Enum value, bool inherit = false) where TAttribute : Attribute
+    {
+        var fieldInfo = value.GetType().GetField(value.ToString());
+        var attributes = fieldInfo.GetCustomAttribute(typeof(TAttribute), inherit);
+        return attributes as TAttribute;
+    }
+}
