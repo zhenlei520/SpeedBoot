43a9a7aa4cec3cdc01915762a8f3309077511f7a
zhenlei520 <wangzhenlei520@gmail.com>
2024-05-19 16:41:07 +0800

    chore: update package version

--- test/SpeedBoot.System.Tests/Repositories/IUserRepository.cs
+++ test/SpeedBoot.System.Tests/Repositories/IUserRepository.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) zhenlei520 All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using SpeedBoot.System.Tests.Models;
-
 namespace SpeedBoot.System.Tests.Repositories;
 
 public interface IUserRepository : IRepository<User>
